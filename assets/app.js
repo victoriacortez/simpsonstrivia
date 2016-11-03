9223c35275bf7a4028b05a0487a5a0f302a97e76
victoria cortez <victoria cortez>
2016-11-03 16:45:53 -0400

    updated timer

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,12 @@
+var score = 0;
 
- $("button").click(function () {
+$("button").click(function () {
         $("#startScreen").hide();
         $(".container").show();
+        time();
     });
 $('.container').hide();
+
 
 var questions = [{
   "question": "What's Bart's Middle Name?",
@@ -54,21 +57,43 @@ $("#opt2").html(questions[x].option2);
 $("#opt3").html(questions[x].option3);
 $("#opt4").html(questions[x].option4);
 
+function next(){
+  $(".display").html(questions[x].question);
+  $("#opt1").html(questions[x].option1);
+  $("#opt2").html(questions[x].option2);
+  $("#opt3").html(questions[x].option3);
+  $("#opt4").html(questions[x].option4);
+  sec = 10;
+  $('.timer').html(sec);
+
+}
+$('.option').click(function(){
+  // console.log("works");
+  x++
+  next();
+})
+//homer audio
 var audio = new Audio('http://www.wavsource.com/snds_2016-10-30_1570758759693582/tv/simpsons/homer/doh1_y.wav');
 //timer
-setInterval(function() {
-  var timer = $('.timer').html();
-  timer = timer.split(':');
-  var seconds = timer[1];
-    if(seconds!="00"){
-      seconds -=1;
-    if(seconds < 0){
-      seconds= 59;
-    }
-    else if (seconds < 10 && length.seconds !=2) seconds = '0' +seconds;
-    $('.timer').html(":" + seconds);
-    }
-},1000);
+function time (){
+  setInterval(timer, 1000);
+}
+
+var sec = 10;
+$('.timer').html(sec);
+function timer(){
+  sec--
+  $('.timer').html(sec);
+  if(sec==0){
+    next();
+  }
+}
+
 audio.play();
 
+var answers = ["0","2","3","0","3"], 
+    tot = answers.length;
 
+function getScore(){
+ 
+}
